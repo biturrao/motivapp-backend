6caa45c8452daadd5879f668749f878239c4710e
biturrao <biturrao@correo.uss.cl>
2025-10-04 18:53:46 -0300

    Fix: Adapta config.py para leer DATABASE_URL

--- app/core/config.py
+++ app/core/config.py
@@ -2,24 +2,13 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # Base de datos
-    DATABASE_USER: str
-    DATABASE_PASSWORD: str
-    DATABASE_NAME: str
-    DATABASE_HOST: str
-    DATABASE_PORT: str
+    # Ahora esperamos la URL completa directamente del entorno
+    DATABASE_URL: str
 
     # JWT
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    @property
-    def DATABASE_URL(self) -> str:
-        """
-        Genera la URL de conexión a la base de datos a partir de las variables de entorno.
-        """
-        return (f"postgresql+psycopg2://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}@"
-                f"{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}")
-
-# Creamos una instancia única de la configuración que se importará en otros módulos
+# Creamos una instancia única de la configuración
 settings = Settings()
